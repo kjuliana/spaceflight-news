e0586d3ecca4f7ce953978f5f0cc227b49a2b8c7
kjuliana <korjuliana@gmail.com>
2022-12-03 20:39:28 +0300

    Комментарии

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,22 +7,40 @@ import Loader from "../components/UI/Loader/Loader";
 const PostIdPage = () => {
     const {postId} = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
+
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id);
         setPost(response.data);
     });
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id);
+        setComments(response.data);
+    });
 
     useEffect(() => {
         fetchPostById(postId);
+        fetchComments(postId);
     }, [])
 
     return (
         <div>
-            Страница поста номер {postId}
+            <h1>Страница поста номер {postId}</h1>
             {
                 isLoading
                     ? <Loader/>
                     : <div>{post.id}. {post.title}</div>
+            }
+            <h2>Комментарии</h2>
+            {isComLoading
+                ? <Loader/>
+                : <div>{comments.map(comm =>
+                    <div style={{marginTop: '15px'}}>
+                        <h5>{comm.email} </h5>
+                        <p> {comm.body} </p>
+                    </div>
+                )}
+                </div>
             }
         </div>
     );
